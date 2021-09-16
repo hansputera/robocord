a400bc1d3054900929994a891cf844f219b5d6d1
hansputera <hanif@tslab.site>
2021-09-16 03:58:50 +0000

    fix(token): remove token

--- src/bot.ts
+++ src/bot.ts
@@ -1,7 +1,7 @@
 import { Client } from "./client";
 import { Util } from "./utils";
 
-const bot = new Client('ODc5MzE5NTcwOTIwNDA3MDQw.YSOAKg.g7EQ7JNoCxMzX5Da3Mw97BVjOGA', {
+const bot = new Client('', {
     intents: [
         Util.intents.GUILD, Util.intents.GUILD_MESSAGES
     ],
